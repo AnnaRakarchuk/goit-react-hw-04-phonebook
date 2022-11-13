b91117328e608ade93187d9f56db52cdc43c4045
Anna Rakarchuk <anytarakarchuk@gmail.com>
2022-11-13 21:06:10 +0200

    refactoring the task of phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
+import { useState } from 'react';
+import { Form } from './Form/Form';
+import { Contacts } from './Contacts/Contacts';
+import { Filter } from './Filter/Filter';
+import { useLocalStorage } from './LocalStorage/LocalStorage';
+import css from './App/App.module.css';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+const [contacts, setContacts] = useLocalStorage('contacts', []);
+const [filter, setFilter] = useState('');
+
+// ========Додає контакти та виводить попередження якщо наявний такий контакт=========
+const onAddingContacts = newContact => {
+  contacts.find(prev => prev.name === newContact.name)
+    ? alert(`${newContact.name} is already in contacts`)
+    : setContacts(prev => [...prev, newContact]);
+      };
+
+  // ===========Фільтр контактів==============
+
+  const onFilterHandler = e => {
+    const { value } = e.currentTarget;
+    setFilter(value);
+  };
+
+    // ===========Видаляє контакти==============
+    const onDeleteHandler = contactId => {
+      const notAid = prev => prev.id !== contactId;
+      const updatedList = contacts.filter(notAid);
+  
+      setContacts(updatedList);
+    };
+
+    //==============Рендер===================
+    return(
+      <div className={css.form__wrapper}>
+        <h2 className={css.form__title}>Phonebook</h2>
+        <Form onAddingContacts={onAddingContacts} contacts={contacts} />
+        <h2 className={css.form__title}>Contacts</h2>
+        <Filter filteredContent={filter} onFilterHandler={onFilterHandler} />
+        <Contacts
+          contacts={contacts}
+          filteredContent={filter}
+          handleDelete={onDeleteHandler}
+        />
+      </div>
+    )
 };
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import css from './Form.module.css';
+import { nanoid } from 'nanoid';
+
+export const Form = ({ onAddingContacts }) =>{
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const ValueHandler = e => {
+    const { name, value } = e.target;
+    switch(name) {
+case 'name':
+  setName(value);
+  break;
+  case 'number':
+    setNumber(value);
+    break;
+    default:
+      break;
+    }
+};
+
+const onSubmitBtn = e => {
+ e.preventDefault();
+
+ const newContact = {
+  id: nanoid(),
+  name: name,
+  number: number,
+};
+
+onAddingContacts(newContact);
+reset();
+};
+
+const reset =() =>{
+  setName('');
+  setNumber('');
+};
+
+    return (
+      <form className={css.form} onSubmit={onSubmitBtn}>
+        <label className={css.form__label}>
+          Name
+          <input
+            className={css.form__input}
+            placeholder="Name"
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={name}
+            onChange={ValueHandler}
+          />
+        </label>
+        <label className={css.form__label}>
+          Number
+          <input
+            className={css.form__input}
+            placeholder="Phone number"
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={number}
+            onChange={ValueHandler}
+          />
+        </label>
+        <button className={css.form__button} type="submit">
+          Add contact
+        </button>
+      </form>
+    );
+}
+
